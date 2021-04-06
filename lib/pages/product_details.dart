069a0a2c2a3192e5252558a6027ef36fe3aa8397
91639 <aryan.goel002@gmail.com>
2021-04-06 15:35:46 +0530

    second commit

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -197,6 +197,44 @@ class _ProductDetailsState extends State<ProductDetails> {
                 color: Colors.red[700],
               )
             ],
+          ),
+          Divider(
+            //height: 13,
+            thickness: 1,
+            color: Colors.black,
+          ),
+          ListTile(
+            title: Text("product details"),
+            subtitle: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: Text(
+                  "Product Name",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: Text(
+                  "Product brand",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: Text(
+                  "Product condition",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
           )
         ],
       ),
