616e42456250633c512901683773e94ba4b912b0
91639 <goel.aryan2406@gmail.com>
2021-04-09 15:21:40 +0530

    inserion of products in cart

--- lib/pages/cart_products.dart
+++ lib/pages/cart_products.dart
@@ -10,48 +10,94 @@ class _CartproductsState extends State<Cartproducts> {
     {
       "name": "Blazer men",
       "picture": "images/cats/products/blazer1.jpeg",
-      "old_price": 250,
       "price": 150,
-    },
-    {
-      "name": "Blazer women",
-      "picture": "images/cats/products/blazer2.jpeg",
-      "old_price": 310,
-      "price": 240,
-    },
-    {
-      "name": "Red dress",
-      "picture": "images/cats/products/dress1.jpeg",
-      "old_price": 200,
-      "price": 140,
-    },
-    {
-      "name": "Black dress",
-      "picture": "images/cats/products/dress2.jpeg",
-      "old_price": 280,
-      "price": 200,
-    },
-    {
-      "name": "Heels",
-      "picture": "images/cats/products/hills1.jpeg",
-      "old_price": 2100,
-      "price": 1400,
-    },
-    {
-      "name": "Heels",
-      "picture": "images/cats/products/hills2.jpeg",
-      "old_price": 2100,
-      "price": 1400,
+      "size": "L",
+      "color": "black",
+      "quantity": 1
     },
   ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return Text("blah");
-      },
+        itemCount: Products_on_the_cart.length,
+        itemBuilder: (context, index) {
+          return SingleCartProduct(
+            cart_product_name: Products_on_the_cart[index]["name"],
+            cart_product_color: Products_on_the_cart[index]["color"],
+            cart_product_qty: Products_on_the_cart[index]["qty"],
+            cart_product_size: Products_on_the_cart[index]["size"],
+            cart_product_picture: Products_on_the_cart[index]["picture"],
+            cart_product_price: Products_on_the_cart[index]["price"],
+          );
+          //cart_product_picture: Products_on_the_cart[index]["picture"]);
+        });
+  }
+}
+
+class SingleCartProduct extends StatelessWidget {
+  final cart_product_name;
+  final cart_product_picture;
+  final cart_product_price;
+  final cart_product_size;
+  final cart_product_color;
+  final cart_product_qty;
+  SingleCartProduct(
+      {this.cart_product_name,
+      this.cart_product_picture,
+      this.cart_product_price,
+      this.cart_product_size,
+      this.cart_product_color,
+      this.cart_product_qty});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          cart_product_name,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        subtitle: Column(
+          children: [
+            Row(
+              children: [
+                //*?this section is fotr the size of the product
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "size:",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(cart_product_size,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(20, 8, 8, 8)),
+
+                //*? this section is for the color of the product
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text("Color:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(cart_product_color,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
